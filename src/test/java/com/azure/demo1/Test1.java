685729baccee0c401b0f5f540d3c7eaf6e082df5
ruturaj kolekar <ruturajkolekar56@Gmail.com>
2022-12-28 17:15:13 +0530

    added Login Tesr

--- src/test/java/com/azure/demo1/Test1.java
+++ src/test/java/com/azure/demo1/Test1.java
@@ -1,11 +1,56 @@
 package com.azure.demo1;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.time.Duration;
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class Test1 {
 
-	@Test
-	public void start() {
-		System.out.println("Hello");
+	WebDriver driver;
+	Properties prop;
+	SoftAssert softAsssert;
+
+	
+	@BeforeMethod
+	public void setup() throws Exception {
+		File file = new File("./src/test/resources/config.properties");
+		System.out.println(file.exists());
+		FileInputStream fis = new FileInputStream(file);
+		prop= new Properties();
+		prop.load(fis);
+
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.get(prop.getProperty("url"));
+		 softAsssert = new SoftAssert();
 	}
+	
+	
+	@Test
+	public void login() {
+		
+		driver.findElement(By.xpath("//input[@name='username']")).sendKeys(prop.getProperty("username"));
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys(prop.getProperty("password"));
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		String Title=driver.getTitle();
+		softAsssert.assertEquals("OrangeHRM",Title);
+	}
+	
+	@AfterMethod
+	public void teardown() throws Exception {
+		Thread.sleep(5000);
+		driver.quit();
+		softAsssert.assertAll();
+	}
+
 }
